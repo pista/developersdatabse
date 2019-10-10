4d3344ce6e43fe7860964561328c6317c240a203
Sven Boekhoff <sven@boekhoff.info>
2019-10-10 23:51:20 +0200

    Remove unused imports.

--- src/main/java/com/example/Developer.java
+++ src/main/java/com/example/Developer.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.Transient;
 
 @Entity
 public class Developer {
